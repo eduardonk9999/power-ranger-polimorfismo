8dca302d25a4e37fe54f67af0816e54f9c2760dd
Eduardo Silva <thiago.novak7@gmail.com>
2023-09-09 14:32:53 +0000

    name

--- src/Adolescente.java
+++ src/Adolescente.java
@@ -1,3 +1,15 @@
 abstract class Adolescente {
-    private adolescente
+    private String name;
+    private String power;
+
+    public Adolescente(String[] args){
+        this.name = name;
+        this.power = power;
+    }
+
+    public void setNome(String nome) {
+        this.name = name;
+        
+    }
+     
 }
