f9cd786f8d2fc744c9813585b479be3698345c11
Eduardo Silva <thiago.novak7@gmail.com>
2023-09-11 18:25:38 -0300

    ranger

---
+++ src/PowerRanger.java
@@ -0,0 +1,5 @@
+public class PowerRanger extends Ranger{
+    public PowerRanger(String name, String power) {
+        super(name, power);
+    }
+}
---
+++ src/Ranger.java
@@ -0,0 +1,14 @@
+abstract class Ranger {
+    private String name;
+    private String power;
+
+    public Ranger(String name, String power){
+        this.name = name;
+        this.power = power;
+    }
+
+    public void morfar() {
+        System.out.println(name + " Hora de morfar");
+     }
+     
+}
